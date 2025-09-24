45e15fa7848f756525e295fa7abc25085612ff42
WhiteWolf <matheodeponte45@gmail.com>
2025-09-24 10:31:22 +0200

    init2

---
+++ _main.py
@@ -0,0 +1,8 @@
+import pygame
+from _painting import Painting
+
+
+class Main:
+
+    def __init__(self):
+        self.painting = Painting()
---
+++ _painting.py
@@ -0,0 +1,7 @@
+import pygame
+
+
+class Painting:
+    
+    def __init__(self, main):
+        self.main = main
